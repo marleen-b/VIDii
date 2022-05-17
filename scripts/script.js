0faf9216e9d6926a17d592fd758baa4744a1df31
marleen-b <70547716+marleen-b@users.noreply.github.com>
2022-05-17 12:53:55 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,58 @@
 // WEGVLIEGEN 
 var startButton = document.querySelector("header button");
+// Spreekt de button aan in de header 
 var cloudGuy = document.querySelector("section.cloudguy");
 var verkeerslicht = document.querySelector("div.verkeerslicht");
+var marioKarakter = document.querySelector("img.mario");
+var bodyBackground = document.querySelector("body");
 
-/*Spreekt de button aan in de header */
-startButton.addEventListener("click", startenEnWegvliegen );
+//Speel functie startenEnWegVliegen na klik op button
+startButton.addEventListener("click", startenEnWegvliegen);
 
-/* Dit om de cloudguy weg te laten vliegen */ 
+// functie om cloudguy en verkeerslicht weg te laten vliegen 
 function startenEnWegvliegen (){
     verkeerslicht.classList.add("verkeerslichtAan");
     cloudGuy.classList.add("cloudGuyWegVliegen");
-}
-/*Mario verplaatsen na klik op een mushroom button*/
-var marioKarakter = document.querySelector("img.mario");
-var mushroomButton1992 = document.querySelector("li:nth-of-type(1) button");
-mushroomButton1992.addEventListener("click", naar1992);
 
-function naar1992(){
- marioKarakter.classList.add("y1992");   
+    // stukje op weg en veranderd achtergrond
+    marioKarakter.classList.add("y1992");
+    bodyBackground.classList.add("y1992");
 }
-/*Met class y1992 in css mario positioneren animeren*/
+
+// Mario verplaatst na klik op een mushroom button
+var mushroomButton2001 = document.querySelector("li:nth-of-type(2) button");
+var mushroomButton2005 = document.querySelector("li:nth-of-type(3) button");
+var mushroomButton2008 = document.querySelector("li:nth-of-type(4) button");
+var mushroomButton2011 = document.querySelector("li:nth-of-type(5) button");
+var mushroomButton2017 = document.querySelector("li:nth-of-type(6) button");
+
+
+// Na klik op mushroom speel functie naarJAARTAL af 
+// Background veranderd na klik op mushroom button
+// Mario verplaatst na klik op een mushroom button
+mushroomButton2001.addEventListener("click", naar2001);
+mushroomButton2005.addEventListener("click", naar2005);
+mushroomButton2008.addEventListener("click", naar2008);
+mushroomButton2011.addEventListener("click", naar2011);
+mushroomButton2017.addEventListener("click", naar2017);
+
+function naar2001(){
+    marioKarakter.classList.add("y2001");   
+    bodyBackground.classList.add("y2001");
+}
+
+function naar2005(){
+    marioKarakter.classList.add("y2005");   
+}
+
+function naar2008(){
+    marioKarakter.classList.add("y2008");   
+}
+
+function naar2011(){
+    marioKarakter.classList.add("y2011");   
+}
+
+function naar2017(){
+    marioKarakter.classList.add("y2017");   
+}
